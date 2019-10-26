2f4a96fda66ef13e2271437cf3fc6e0fafb58139
MartinCayuelas <martincayuelas@gmail.com>
2019-10-26 18:08:25 +0200

    [ADD] new routes for IG and DO

--- src/routes/do.ts
+++ src/routes/do.ts
@@ -7,4 +7,15 @@ doRouter.get('/', (req: any, res: any) => {
   res.json('DO section!');
 });
 
+
+//Get a text from the API and send it
+doRouter.get('/presentation', (req: any, res: any) => {
+  res.type('application/json');
+  res.status(200);
+  res.json('Présentation DO!');
+});
+
+
+
+
 module.exports = doRouter;
