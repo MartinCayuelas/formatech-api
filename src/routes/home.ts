dceabb9172840c2875355bc402f74cc9fb5d5002
Fatima <fatima.machhouri1@gmail.com>
2019-10-27 12:46:17 +0100

    [DONE] Add cors to enable the connection between front/back

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,7 +1,8 @@
 const homeRouter = require('express').Router();
+var cors = require('cors');
 
 //Get a text from the API and send it
-homeRouter.get('/', (req: any, res: any) => {
+homeRouter.get('/', cors(), (req: any, res: any) => {
   res.type('application/json');
   res.status(200);
   res.json('Accueil depuis le back!');
