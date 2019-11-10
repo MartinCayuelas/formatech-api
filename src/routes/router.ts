4780a919f17db4c2998e0afe2abbca2f8cd7bdfa
root <raphael.luciano@outlook.fr>
2019-11-10 13:09:48 +0100

    [FIX] try to resolve merge conflict

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -6,6 +6,7 @@ import igRoute from './ig';
 import doRoute from './do';
 import contactRoute from './contact';
 import userRoute from './user';
+import sagesseRoute from './sagesse';
 
 // Routes
 router.use('/accueil', homeRoute);
@@ -13,5 +14,6 @@ router.use('/ig', igRoute);
 router.use('/do', doRoute);
 router.use('/contact', contactRoute);
 router.use('/utilisateur', userRoute);
+router.use('/sagesse', sagesseRoute);
 
 export default router;
