eebbbc45d01fd427e3ac75a0d5d70cdad0fe02db
root <raphael.luciano@outlook.fr>
2019-11-10 14:33:34 +0100

    [FIX] try to resolve merge error

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,6 +5,7 @@ import homeRoute from './home';
 import igRoute from './ig';
 import doRoute from './do';
 import contactRoute from './contact';
+import userRoute from './user';
 import sagesseRoute from './sagesse';
 
 // Routes
@@ -12,6 +13,7 @@ router.use('/accueil', homeRoute);
 router.use('/ig', igRoute);
 router.use('/do', doRoute);
 router.use('/contact', contactRoute);
+router.use('/utilisateur', userRoute);
 router.use('/sagesse', sagesseRoute);
 
 export default router;
