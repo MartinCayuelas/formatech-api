39a61c2637331544450a008e1b8dc2672249a048
Martin Cayuelas <martincayuelas@gmail.com>
2019-11-14 22:38:56 +0100

    Update sagesse_database.ts

--- src/config/sagesse_database.ts
+++ src/config/sagesse_database.ts
@@ -1,4 +1,4 @@
 import Sequelize from 'sequelize';
 
 //export default db = new Sequelize(process.env.SAGESSE_DATABASE_URL!);
-export const db = new Sequelize.Sequelize(process.env.SAGESSE_DATABASE_URL!,{});
+export const db = new Sequelize.Sequelize('postgres://'+process.env.SAGESSE_USER!+':'+process.env.SAGESSE_PASSWORD!+'@'+process.env.SAGESSE_HOST!+':'+process.env.SAGESSE_PORT!+'/'+process.env.SAGESSE_DATABASE_NAME!,{});
